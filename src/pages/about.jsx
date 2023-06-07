943e8eb1d62166bbdba49f59ad28f97bc709e8a3
Nastja <100337678+sobnastja@users.noreply.github.com>
2023-06-07 14:27:31 +0300

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div>about</div>
+    <div>On our website of culinary recipes "React", we have tried to collect most of the simple recipes of the most delicious, affordable, popular and delicious dishes. A detailed step-by-step description of the cooking technology will help you, not only quickly and without any problems, prepare the dish you like, but also easily master the basics of cooking. Each recipe has additions and recommendations for the preparation and decoration of the finished dish.</div>
   )
 }
 
